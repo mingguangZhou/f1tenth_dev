05a4861d89e41d715cc2f031d55e5e87b5357078
mingguangZhou <mingguang_zhou@berkeley.edu>
2025-09-26 13:08:29 +0200

    Sync file structure as on vehicle and update master launch file

--- path_following/path_following/launch/path_following_launch.py
+++ path_following/path_following/launch/path_following_launch.py
@@ -1,17 +1,35 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+<<<<<<< HEAD
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+=======
+>>>>>>> 4dd6227 (Sync file structure as on vehicle and update master launch file)
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
+<<<<<<< HEAD
     # Get the package directory
     pkg_share = get_package_share_directory('path_following')
     
     # Define the config file path
     config_file = os.path.join(pkg_share, 'config', 'planner_params.yaml')
     
+=======
+    # Config file paths
+    drive_arbitration_config = os.path.join(
+        get_package_share_directory('drive_arbitration'),
+        'config',
+        'drive_arbitration_params.yaml'
+    )
+    safety_bubble_config = os.path.join(
+        get_package_share_directory('safety_bubble'),
+        'config',
+        'params.yaml'
+    )
+
+>>>>>>> 4dd6227 (Sync file structure as on vehicle and update master launch file)
     return LaunchDescription([
         Node(
             package='path_following',
@@ -39,11 +57,29 @@ def generate_launch_description():
             output='screen'
         ),
 
-        # Boundary detection node
+        # --- Boundary detection
         Node(
             package='boundary_detection',
             executable='boundary_detection_node',
             name='boundary_detector',
             output='screen'
+        ),
+
+        # --- Safety bubble (with yaml config)
+        Node(
+            package='safety_bubble',
+            executable='safety_bubble_node',
+            name='safety_bubble',
+            output='screen',
+            parameters=[safety_bubble_config]
+        ),
+
+        # --- Drive arbitration (with yaml config)
+        Node(
+            package='drive_arbitration',
+            executable='drive_arbitrator_node',
+            name='drive_arbitrator',
+            output='screen',
+            parameters=[drive_arbitration_config]
         )
     ])
